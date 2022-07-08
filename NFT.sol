f5e3bd4ef02b56e5076535d779ef4545faca0304
Jaca28 <42952652+Jaca28@users.noreply.github.com>
2022-07-08 15:37:13 -0500

    Create NFT.sol

---
+++ NFT.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract contract token{
+  function mint(address account, uint256 amount)public virtual;
+  function transferFrom ( address from, address to, uint256 amount)public virtual;
+  function transfer(address to, uint256 amount)public virtual;
+  function balanceOf(address account)public virtual returns(uint256);
+  function totalSupply()public virtual returns(uint256);
+}
+
+contract NFT is ERC721Enumerable, Ownable {
+  using Strings for uint256;
+
+  token t;
+  uint id;//token id 
+  string public baseURI;
+  string public baseExtension = ".json";
+  string public notRevealedUri;
+  uint256 public cost = 1 ether;
+  uint256 public maxSupply = 10000;
+  uint256 public maxMintAmount = 20;
+  uint256 public nftPerAddressLimit = 3;
+  bool public paused = false;
+  bool public revealed = false;
+  bool public onlyWhitelisted = true;
+  address[] public whitelistedAddresses;
+  mapping(address => uint256) public addressMintedBalance;
+  mapping(address => uint) public tokenAddrID;
+  mapping(address => uint) public tokenAddrNftValue;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI,
+    string memory _initNotRevealedUri
+  ) ERC721(_name, _symbol) {
+    setBaseURI(_initBaseURI);
+    setNotRevealedURI(_initNotRevealedUri);
+    id=1;
+  }
+
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  // public
+  function mint(uint256 _mintAmount, uint _amount, address _tokenPaymentAddr) public {
+    require(!paused, "the contract is paused");
+    uint256 supply = totalSupply();
+    require(_mintAmount > 0, "need to mint at least 1 NFT");
+    require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
+    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+    uint nftValue = tokenAddrNftValue[_tokenPaymentAddr];
+    t = token(_tokenPaymentAddr);
+    uint actualBalance = t.balanceOf(msg.sender);
+    require (actualBalance >= _amount, "insufficient funds");
+    require(_amount >= nftValue * _mintAmount, "insufficient funds");
+    t.transferFrom(msg.sender, address(this), _amount);
+   
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      addressMintedBalance[msg.sender]++;
+      _safeMint(msg.sender, supply + i);
+    }
+    
+  }
+
+
+  function setMethodPayment(address _tokenAddr, uint _nftValue) public onlyOwner{
+    tokenAddrID[_tokenAddr]=id;
+    tokenAddrNftValue[_tokenAddr] = _nftValue;
+    id += id;
+  }
+  
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+    
+    if(revealed == false) {
+        return notRevealedUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
+  }
+
+  //only owner
+  function reveal() public onlyOwner {
+      revealed = true;
+  }
+  
+  function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
+    nftPerAddressLimit = _limit;
+  }
+  
+  function setCost(uint256 _newCost) public onlyOwner {
+    cost = _newCost;
+  }
+
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+    maxMintAmount = _newmaxMintAmount;
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+  
+  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+    notRevealedUri = _notRevealedURI;
+  }
+
+  function pause(bool _state) public onlyOwner {
+    paused = _state;
+  }
+  
+  function withdraw() public payable onlyOwner {
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+  }
+}
